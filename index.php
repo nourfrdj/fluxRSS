50af19ea669b8ba89796b1ee67fd6432abb6a43b
nourhene <nourhene.fredj@gmail.com>
2021-02-09 14:10:02 +0100

    fenetre parametre pas encore finit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php SESSION_start() ?>
-<?php require("view/header.php"); 
+<?php require("view/header.php");
 require("controllers/flux_controllers.php");
 $url = "https://www.01net.com/rss/actualites/produits/";
 $_SESSION['nbreacticlechoisi'] = 20;
@@ -8,48 +8,51 @@ $idmodal = 0;
 ?>
 <div class="ndContent">
   <div class="ndContCard">
-    <?php for ($counter = 0; $counter < $_SESSION['nbreacticlechoisi']; $counter++) { 
+    <?php for ($counter = 0; $counter < $_SESSION['nbreacticlechoisi']; $counter++) {
       $idmodal++;
-      ?>
-    <div class="ndCard">
-      <div class="circle" style="background-image:url('<?= $_SESSION['tableauArticles'][$counter]['img'] ?>')">
-      </div>
-      <div class="content">
-        <h4><?= $_SESSION['tableauArticles'][$counter]['title'] ?></h4>
-        <p><?= formatdate($_SESSION["tableauArticles"][$counter]['date']) ?></p>
-        <a href="#" data-bs-toggle="modal" data-bs-target="#modal<?= $idmodal ?>">read more</a>
-      </div>
-    </div>
-
-    <!-- la modal pour chaque article-->
-    <div class="modal fade" id="modal<?= $idmodal ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">
-                        <?=$_SESSION['tableauArticles'][$counter]['title']?>
-                        <button type="close" class="btn btn-danger" data-bs-dismiss="modal">X</button>                       
-                        </h5>
-                    </div>
-                    <div class="modal-body">
-                        <?= formatdescription($_SESSION["tableauArticles"][$counter]['desc']); ?>
-                    </div>
-                    <div class="modal-footer">
-                    <button type="boutton" class="btn btn-primary" data-bs-dismiss="modal">lire l'article</button>
-                    </div>
-                </div>
-            </div>
+    ?>
+      <div class="ndCard">
+        <div class="circle" style="background-image:url('<?= $_SESSION['tableauArticles'][$counter]['img'] ?>')">
         </div>
+        <div class="content">
+          <h4><?= $_SESSION['tableauArticles'][$counter]['title'] ?></h4>
+          <p><?= formatdate($_SESSION["tableauArticles"][$counter]['date']) ?></p>
+          <a href="#" data-bs-toggle="modal" data-bs-target="#modal<?= $idmodal ?>">read more</a>
+        </div>
+      </div>
+
+      <!-- la modal pour chaque article-->
+      <div class="modal fade" id="modal<?= $idmodal ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                <?= $_SESSION['tableauArticles'][$counter]['title'] ?>
+                <button type="close" class="btn btn-danger" data-bs-dismiss="modal">X</button>
+              </h5>
+            </div>
+            <div class="modal-body">
+              <?= formatdescription($_SESSION["tableauArticles"][$counter]['desc']); ?>
+            </div>
+            <div class="modal-footer">
+              <button type="boutton" class="btn btn-primary" data-bs-dismiss="modal">lire l'article</button>
+              <div class="bi bi-facebook" data-href="" data-layout="button" data-size="small">Partager cet article :<a target="" href="https://www.facebook.com/sharer/sharer.php?u=https://www.01net.com/rss/actualites/produits/src=sdkpreparse" class="fb-xfbml-parse-ignore"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                  </svg></a></div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     <?php
-        }
-          ?>
-
+    }
+    ?>
   </div>
 </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- view/header.php
+++ view/header.php
@@ -1,5 +1,9 @@
-<!doctype html>
-<html lang="fr">
+  <?php
+  setcookie('themeWhite', 'themeWhite', time() + 365 * 24 * 3600); //cookie pour 1 an
+  ?>
+  <!doctype html>
+  <html lang="fr">
+
   <head>
     <!-- Required meta tags -->
     <title>Flux RSS</title>
@@ -8,22 +12,26 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/theme1.css">
     <title>Hello, world!</title>
   </head>
+
   <body>
     <div class="ndNav">
-            <div class="ndColNav">
-            <a href="index.php"><p>test</p></a>
-            </div>
-            <div class="ndColNav" id="ndLogo">
-                <a href="index.php"><img  src="../assets/img/.."/></a>
-            </div>
-            <div class="ndColNav" id="ndParam">
-                <a href="param.php"><i class="fa fa-cog"></i></a>
-            </div>
+      <div class="ndColNav">
+        <a href="index.php">
+          <p>test</p>
+        </a>
+      </div>
+      <div class="ndColNav" id="ndLogo">
+        <a href="index.php"><img src="../assets/img/.." /></a>
+      </div>
+      <div class="ndColNav" id="ndParam">
+        <form method="GET">
+          <a href="" data-bs-toggle="modal" data-bs-target="#idmodalparametre"><i class="fa fa-cog"></i></a>
+        </form>
+      </div>
     </div>
     <div class="entete">
       <div class="slog">
@@ -31,3 +39,60 @@
         <p>Toute l'actualité dans le creux de votre main</p>
       </div>
     </div>
+    </div>
+    <div id="idmodalparametre" class="modal" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header ">
+            <h5 class="modal-title ">Parametre</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="d-flex align-items-start">
+              <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Apparence</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">la gestion des flux </a>
+                <a class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+              </div>
+              <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                  <div>
+                    <label for="theme">Choisissez votre Theme : </label>
+                    <p> <input type="radio" id="themeWhite" name="theme" value="themeWhite" <?= isset($_COOKIE['theme']) == 'themeWhite' ? 'checked' : '' ?>><label for="themeWhite">Jour</label></p>
+                    <p>
+                      <input type="radio" id="themeBlack" name="theme" value="themeBlack" <?= isset($_COOKIE['theme']) == 'themeBlack' ? 'checked' : '' ?>><label for="themeBlack">Nuit</label>
+                    </p>
+                    <p>
+                      <input type="radio" id="themeRJB" name="theme" value="RJB" <?= isset($_COOKIE['theme']) == 'RJB' ? 'checked' : '' ?>>
+                      <label for="themeRJB">Technologie</label>
+                    </p>
+
+                  </div>
+
+                </div>
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                  <div class="">  <label for="cathegory">Choisissez les cathegorie que vous voulez suivre</label>
+                    <p><input type="checkbox" id="cathegory" name="applis-logiciels">
+                    <label for="cathegory">applis-logiciels</label></p>
+                    <p><input type="checkbox" id="cathegory" name="technologie">
+                    <label for="cathegory">Technologie</label></p>
+                    <p><input type="checkbox" id="cathegory" name="Jeux">
+                    <label for="cathegory">Jeux</label></p>
+                    <p><input type="checkbox" id="cathegory" name="Security">
+                    <label for="cathegory">Technologie</label></p>
+                    <p><input type="checkbox" id="cathegory" name="Products">
+                    <label for="cathegory">Produits</label></p>
+                  </div>
+                </div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Enregistrer les modifications</button>
+              <button type="close" class="btn btn-primary">Fermer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
